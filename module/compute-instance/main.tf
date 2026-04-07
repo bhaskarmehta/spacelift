ca366b3482c2b67abff6d41a8e007094f388512e
Bhaskar Mehta <46514777+bhaskarmehta@users.noreply.github.com>
2026-04-07 15:21:01 +0530

    Create main.tf

---
+++ module/compute-instance/main.tf
@@ -0,0 +1,24 @@
+resource "google_compute_instance" "default" {
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  tags = var.tags
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network    = var.vpc_id
+    subnetwork = var.subnet_id
+
+    access_config {}
+  }
+}
