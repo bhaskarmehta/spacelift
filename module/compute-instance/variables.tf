f274ee912b512c4938814c00f3dfbf6b0c16df6f
Bhaskar Mehta <46514777+bhaskarmehta@users.noreply.github.com>
2026-04-07 15:21:49 +0530

    Create variables.tf

---
+++ module/compute-instance/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {}
+variable "machine_type" {}
+variable "zone" {}
+
+variable "vpc_id" {}
+variable "subnet_id" {}
+
+variable "tags" {
+  default = []
+}
+
+variable "image" {
+  default = "debian-cloud/debian-11"
+}
