3f81db53f32a8ca581026d349c7bac347bf221d6
Bhaskar Mehta <46514777+bhaskarmehta@users.noreply.github.com>
2026-04-07 15:49:35 +0530

    Update main.tf

--- tf/stage/kdss/vm/main.tf
+++ tf/stage/kdss/vm/main.tf
@@ -1,36 +1,54 @@
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
-  machine_type = "n2-standard-2"
-  zone         = "us-central1-a"
+#resource "google_compute_instance" "default" {
+# name         = "my-instance"
+#  machine_type = "n2-standard-2"
+#  zone         = "us-central1-a"
 
-  tags = ["foo", "bar"]
+#  tags = ["foo", "bar"]
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
-    }
-  }
+#  boot_disk {
+#    initialize_params {
+#      image = "debian-cloud/debian-11"
+#      labels = {
+#       my_label = "value"
+#      }
+#    }
+#  }
 
   # Local SSD disk
-  scratch_disk {
-    interface = "NVME"
-  }
+#  scratch_disk {
+#    interface = "NVME"
+#  }
 
-  network_interface {
-    network    = var.vpc_id
-    subnetwork = var.subnet_id
+#  network_interface {
+#    network    = var.vpc_id
+#    subnetwork = var.subnet_id
 
-    access_config {
+ #   access_config {
       # Ephemeral public IP
-    }
-  }
-}
+  #  }
+#  }
+#}
 
 
 # Variables
 
-variable "vpc_id" {}
-variable "subnet_id" {}
+#variable "vpc_id" {}
+#variable "subnet_id" {}
+
+
+
+
+#############################################
+
+module "compute_instance" {
+  source  = "app.spacelift.io/bhaskarmehta/compute-instance/google"
+  version = "1.0.0"
+
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  vpc_id       = var.vpc_id
+  subnet_id    = var.subnet_id
+}
+
