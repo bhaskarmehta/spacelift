f63be041a3e2f4be9dcba0f7f2eea9494f1c56bb
Bhaskar Mehta <46514777+bhaskarmehta@users.noreply.github.com>
2026-04-09 16:13:06 +0530

    Add Spacelift configuration for multiple stacks

---
+++ spacelift-stack/main.tf
@@ -0,0 +1,35 @@
+provider "spacelift" {}
+
+resource "spacelift_space" "my_space" {
+  name = "my-space"
+}
+
+resource "spacelift_stack" "network" {
+  name          = "network-stack"
+  repository    = "bhaskarmehta/spacelift"
+  branch        = "main"
+  project_root  = "tf/stage/kdss/network"
+  space_id      = spacelift_space.test_space.id
+}
+
+resource "spacelift_stack" "gcs" {
+  name          = "gcs-stack"
+  repository    = "bhaskarmehta/spacelift"
+  branch        = "main"
+  project_root  = "tf/stage/kdss/gcs"
+  space_id      = spacelift_space.my_space.id
+}
+
+resource "spacelift_stack" "vm" {
+  name          = "vm-stack"
+  repository    = "bhaskarmehta/spacelift"
+  branch        = "main"
+  project_root  = "tf/stage/kdss/vm"
+  space_id      = spacelift_space.test_space.id
+}
+
+resource "spacelift_stack_dependency" "vm_dep" {
+  stack_id            = spacelift_stack.vm.id
+  depends_on_stack_id = spacelift_stack.network.id
+}
+
