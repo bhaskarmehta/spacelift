dc170c241ee4fcb3166a065de98d32d26f502fad
Bhaskar Mehta <46514777+bhaskarmehta@users.noreply.github.com>
2026-04-18 20:01:35 +0530

    Refactor compute instance and disk resource definitions

--- module/compute-instance/main.tf
+++ module/compute-instance/main.tf
@@ -3,16 +3,23 @@ provider "google" {
   region  = "us-central1"
 }
 
-data "google_compute_disk" "existing_disk" {
+resource "google_compute_disk" "disk" {
   name = "spacelift-disk"
+  type = "pd-balanced"
   zone = "us-central1-a"
+  size = 15
+
+  labels = {
+    managed_by = "spacelift"
+    env        = "dev"
+  }
 }
 
 resource "google_compute_instance" "default" {
   #name         = var.name
   #machine_type = var.machine_type
   #zone         = var.zone
-  name         = "spacelift-vm"
+  name         = "spacelift-vm-new"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
@@ -37,7 +44,7 @@ resource "google_compute_instance" "default" {
 }
 
 resource "google_compute_attached_disk" "attach_disk" {
-  disk     = data.google_compute_disk.existing_disk.id
+  disk = google_compute_disk.disk.id
   instance = google_compute_instance.default.id
 
   mode = "READ_WRITE"
