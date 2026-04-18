ea1968576a7ec8543fa35a033a0fb3ace2916b46
Bhaskar Mehta <46514777+bhaskarmehta@users.noreply.github.com>
2026-04-18 19:20:25 +0530

    Add existing disk and attach it to compute instance

--- module/compute-instance/main.tf
+++ module/compute-instance/main.tf
@@ -1,3 +1,8 @@
+data "google_compute_disk" "existing_disk" {
+  name = "spacelift-disk"
+  zone = "us-central1-a"
+}
+
 resource "google_compute_instance" "default" {
   name         = var.name
   machine_type = var.machine_type
@@ -21,4 +26,11 @@ resource "google_compute_instance" "default" {
 
     access_config {}
   }
+}
+
+resource "google_compute_attached_disk" "attach_disk" {
+  disk     = data.google_compute_disk.existing_disk.id
+  instance = google_compute_instance.default.id
+
+  mode = "READ_WRITE"
 }
