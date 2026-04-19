6ab37f869836cb86720fbd05edd57fdf0da78995
Bhaskar Mehta <46514777+bhaskarmehta@users.noreply.github.com>
2026-04-19 15:51:54 +0530

    Add disks variable for compute instance module

--- module/compute-instance/variables.tf
+++ module/compute-instance/variables.tf
@@ -5,6 +5,21 @@
 #variable "vpc_id" {}
 #variable "subnet_id" {}
 
+variable "disks" {
+  default = {
+    disk1 = {
+      name = "disk-1"
+      zone = "us-central1-a"
+      size = 11
+    }
+    disk2 = {
+      name = "disk-2"
+      zone = "us-central1-b"
+      size = 12
+    }
+  }
+}
+
 variable "tags" {
   default = []
 }
